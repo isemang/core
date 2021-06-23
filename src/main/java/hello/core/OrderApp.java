75ecc1fddc9aa4bf6b193b7e69ee86103f5a3daa
isemang <a55306039@gmail.com>
2021-06-23 19:40:08 +0900

    [2021.06.23]박민진 - Spring 기본편 인프런 강의 실습

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = "+order);
+    }
+}
